8d584e1ae195adcb3fb0df20105a397d3498e5e7
Mary Chastain <mmchast2@Marys-MacBook-Pro.local>
2017-10-25 19:21:30 -0400

    login files

---
+++ src/login/Login.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactSignupLoginComponent from 'react-signup-login-component';
+ 
+const Login = (props) => {
+    const signupWasClickedCallback = (data) => {
+      console.log(data);
+      alert('Signup callback, see log on the console to see the data.');
+    };
+    const loginWasClickedCallback = (data) => {
+      console.log(data);
+      alert('Login callback, see log on the console to see the data.');
+    };
+    const recoverPasswordWasClickedCallback = (data) => {
+      console.log(data);
+      alert('Recover password callback, see log on the console to see the data.');
+    };
+    return (
+        <div>
+            <ReactSignupLoginComponent
+                title="Signup/ Login"
+                handleSignup={signupWasClickedCallback}
+                handleLogin={loginWasClickedCallback}
+                handleRecoverPassword={recoverPasswordWasClickedCallback}
+            />
+        </div>
+    );
+};
+ 
+export default Login;
