d95ff6b21042da0576ab1b663f6f8b8512be6494
Ezha Randall <ezha.randall@gmail.com>
2017-10-25 21:37:44 -0400

    idk wtf

--- src/src/index.js
+++
@@ -1,9 +0,0 @@
-import 'babel-polyfill';
-import React from 'react';
-import App from './App'; // app components
-import { launch } from '@extjs/reactor';
-import * as d3 from 'd3';
-
-window.d3 = d3; // needed for ExtReact/D3 components
-
-launch(<App/>);
